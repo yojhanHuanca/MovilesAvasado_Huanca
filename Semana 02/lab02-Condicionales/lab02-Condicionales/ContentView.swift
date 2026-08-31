31cc89ceeefbf00a500b2d0d3b4ab3535fefb08a
yojhan.huanca <yhuancayucra@gmail.com>
2026-08-31 16:37:27 -0500

    feat: Crear proyecto Lab02 y Playground en Semana02

---
+++ Semana 02/lab02-Condicionales/lab02-Condicionales/ContentView.swift
@@ -0,0 +1,24 @@
+‹//
+//  ContentView.swift
+//  lab02-Condicionales
+//
+//  Created by Tecsup on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Semana 02/lab02-Condicionales/lab02-Condicionales/lab02_CondicionalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab02_CondicionalesApp.swift
+//  lab02-Condicionales
+//
+//  Created by Tecsup on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct lab02_CondicionalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
